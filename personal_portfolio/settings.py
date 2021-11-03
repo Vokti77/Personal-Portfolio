9400881cde74b52546e18a64d68104d30d34cab3
Voktibikash Roy <vokti77@gmail.com>
2021-11-03 21:30:08 +0600

    ok

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import os
 import django_heroku
-# import dj_database_url
-# from decouple import config
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
